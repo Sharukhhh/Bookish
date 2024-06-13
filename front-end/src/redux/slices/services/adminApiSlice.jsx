3ffb6897ac8718899038df082c7ba464961c59d8
Sharuk <msharuk.b10@gmail.com>
2024-06-13 20:03:46 +0530

    checking hosted services

--- front-end/src/redux/slices/services/adminApiSlice.jsx
+++ front-end/src/redux/slices/services/adminApiSlice.jsx
@@ -1,7 +1,8 @@
 import { createApi , fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:4000/api/admin/',
+    // baseUrl: 'http://localhost:4000/api/admin/',
+    baseUrl: 'https://bookish-8j43.onrender.com/admin/',
 
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('adminToken');
